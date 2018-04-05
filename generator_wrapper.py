27f8588aacf2f7aca69d95ff6dea26a3b204eaab
iq100 <junkyung_kim@brown.edu>
2018-04-05 01:34:38 -0400

    almost done

--- generator_wrapper.py
+++ generator_wrapper.py
@@ -11,7 +11,7 @@ class Args:
                  circle=False, curr_radius=4, zero_ecc=False, just_display=False, window_size=[256,256],
                  contour_length=15, n_images = 200000, shear_val=0, color_path = '',
                  contrast_range=[0.7, 1.], dist_uniform=False, scale_contrast=0.0,
-                 random_contrast=True, random_contrast_std=-1, pause_display=False, distractor_contrast=1.0,
+                 random_contrast=False, random_contrast_std=-1, pause_display=False, distractor_contrast=1.0,
                  global_spacing = 0.25, save_images=False, randomContour=False, paddle_length=1.0,
                  color = False, random_rotations=False, skew_slack=2, zigzag=False, zigzagAngle=0):
 
@@ -47,37 +47,53 @@ t = time.time()
 args = Args()
 
 ## CONSTANTS
-dataset_root = './' #'/gpfs/data/tserre/data/gilbert_contours/'
-args.contour_length = 15
-args.n_images = 5
+num_machines = int(sys.argv[1])
+args.batch_id = int(sys.argv[2])
+total_images = int(sys.argv[3])
+args.n_images = total_images/num_machines
 
+dataset_root = './' #'/gpfs/data/tserre/data/gilbert_contours/'
+
+args.contour_length = 15
 args.paddle_length = 0.1
 args.random_rotations = True
 args.skew_slack = 6.5
 
-## EXPERIMENTAL VARIABLES
-# DREW:
-# uniform distractor contrast from some -1 to 1
-# all paddle contrast with a distribution sigma from -1 to 0.7
-# snake inter-paddle angle from 0 to 20
-# snake inter-paddle distance from -0.7 to 0.7
-num_machines = int(sys.argv[1])
-i_machine = int(sys.argv[2])
-batches = num_machines
-ibatch = i_machine
-lengths = [5]
-args.n_images = 10
+################################# DS: BASELINE
+dataset_subpath = 'baseline'
+gilbert_contours.from_wrapper(args)
+args.contour_path = os.path.join(dataset_root, dataset_subpath)
+gilbert_contours.from_wrapper(args)
 
-for ivalue, value in enumerate(lengths):
-
-    args.contour_length = value
-    dataset_subpath = 'length' + str(value)
+################################# DS: uniform distractor contrast from some -1 to 1
+for dc in [-1.0, -0.5, 0.0, 0.5]:
+    dataset_subpath = 'dist_contrast_' + str(dc)
     args.contour_path = os.path.join(dataset_root, dataset_subpath)
-    args.window_size=[128,128]
-    args.batch_id = ibatch
+    args.distractor_contrast = dc
     gilbert_contours.from_wrapper(args)
+args.distractor_contrast = 1.0
 
+################################# DS: all paddle contrast with a distribution sigma from -1 to 0.7
+args.random_contrast = True
+#for pc in [??]:
+    #?????
+    #contrast_range
+    #dist_uniform
+    #scale_contrast
+    #random_contrast
+    #random_contrast_std = -1
+args.random_contrast = False
 
+################################# DS: snake inter-paddle angle from 0 to 20
+# NOT IMPLEMENTED YET
+
+################################# DS: snake inter-paddle distance from -0.7 to 0.7
+for sv in [-0.7, -0.35, 0.35, 0.7]:
+    dataset_subpath = 'shear_val_' + str(sv)
+    args.contour_path = os.path.join(dataset_root, dataset_subpath)
+    args.shear_val = sv
+    gilbert_contours.from_wrapper(args)
+args.shear_val = 0
 
 elapsed = time.time() - t
 
